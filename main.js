ca7a95c404cd11489ce7b55aaaf94547b4539a19
divyanshu <divyanshusah44@gmail.com>
2026-07-17 16:00:16 +0530

    run n times

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-function recursion(){
-    console.log(20)
-    // recursion()
-}
+// function recursion(){
+//     console.log(20)
+//     // recursion()
+// }
 
-recursion()
+// recursion()
 
+// run n times 
+// let count = 4;
+
+// function run(){
+//     if(count === 4){
+//         console.log(`function completed ${count}`)
+//         return
+//     }
+//     else{
+//         count++
+//         run()
+//     }
+// }
+
+// run()
